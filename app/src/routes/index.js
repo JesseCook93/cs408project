ab2d03aa25076ca12b726e9953567be67672a390
JesseCook93 <135049843+JesseCook93@users.noreply.github.com>
2026-03-14 21:23:44 +0000

    index.js comment updated

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home (landing) page. */
 router.get('/', function(req, res, next) {
-  res.render('landing', { title: 'Welcome to Boards!', description: 'Share anything you wish with your local community or group.With your own instance of Boards, you can share anything you wish with your local community or group. Simply sharae this with whoever you wish, and hit "Get Started" to begin posting!'});
+  res.render('landing', { title: 'Welcome to Boards!', description: 'Share anything you wish with your local community or group. With your own instance of Boards, you can share anything you wish with your local community or group. Simply share this with whoever you wish, and hit "Get Started" to begin posting!'});
 });
 
 /* GET news page. */
@@ -16,7 +16,7 @@ router.get('/post', function(req, res, next) {
   res.render('post', { title: 'Post', description: 'Placeholder (Will be removed in future checkpoints)' });
 });
 
-/* GET details page. */
+/* GET postdetails page. */
 router.get('/postdetails', function(req, res, next) {
   res.render('postdetails', { title: 'Title', description: 'Placeholder (Will be removed in future checkpoints)' });
 });
