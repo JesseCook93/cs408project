1fde03ef3cd21489019d762dfd71c2058db60c86
JesseCook93 <135049843+JesseCook93@users.noreply.github.com>
2026-04-25 19:19:34 +0000

    'Get Started Button' test

--- app/tests/landing.spec.js
+++ app/tests/landing.spec.js
@@ -7,3 +7,11 @@ test.describe('Landing Page', () => {
     await expect(page.locator('p')).toContainText('Share anything you wish with your local community or group. With your own instance of Boards, you can share anything you wish with your local community or group. Simply share this with whoever you wish, and hit "Get Started" to begin posting!');
   });
 });
+
+test.describe('Get Started Button', () => {
+  test('should navigate to /postdetails when Get Started button is clicked', async ({ page }) => {
+    await page.goto('/');
+    await page.getByText('Get Started').click();
+    await expect(page).toHaveURL('/postdetails');
+  });
+});
