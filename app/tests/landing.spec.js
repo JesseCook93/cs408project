ccd85642ba83d39be4cb0bba3825007f804ca2f0
JesseCook93 <135049843+JesseCook93@users.noreply.github.com>
2026-04-25 19:47:04 +0000

    Landing page Tests Added and Passing

--- app/tests/landing.spec.js
+++ app/tests/landing.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+// Tests for the landing page
 test.describe('Landing Page', () => {
   test('should display landing page', async ({ page }) => {
     await page.goto('/');
@@ -8,10 +9,35 @@ test.describe('Landing Page', () => {
   });
 });
 
-test.describe('Get Started Button', () => {
-  test('should navigate to /postdetails when Get Started button is clicked', async ({ page }) => {
+test.describe('Header', () => {
+  test('should have working navigation links', async ({ page }) => {
     await page.goto('/');
-    await page.getByText('Get Started').click();
-    await expect(page).toHaveURL('/postdetails');
+    await expect(page.getByRole('link', { name: 'Boards'})).toBeVisible();
+    await expect(page.getByRole('link', { name: 'News'})).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Post'})).toBeVisible();
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Boards'}).click()
+    await expect(page).toHaveURL('/');
+    await page.goto('/');
+    await page.getByRole('link', { name: 'News'}).click()
+    await expect(page).toHaveURL('/news');
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Post'}).click()
+    await expect(page).toHaveURL('/post');
+  });
+});
+
+test.describe('Get Started Button', () => {
+  test('should navigate to /post when Get Started button is clicked', async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Get Started'}).click()
+    await expect(page).toHaveURL('/post');
+  });
+});
+
+test.describe('Footer', () => {
+  test('should display footer with correct text', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.locator('footer')).toContainText('© 2026 Boards');
   });
 });
